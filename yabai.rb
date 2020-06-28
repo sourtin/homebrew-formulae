b09397593504807e7bb358d124028c631f877a63
William Earley <nitrous@sourt.in>
2020-06-28 14:33:30 +0100

    first commit: yabai fork

---
+++ yabai.rb
@@ -0,0 +1,60 @@
+class Yabai < Formula
+  desc "A tiling window manager for macOS based on binary space partitioning. [sourtin's fork, head-only]"
+  homepage "https://github.com/sourtin/yabai"
+  head "https://github.com/sourtin/yabai.git"
+
+  depends_on :macos => :high_sierra
+
+  def install
+    (var/"log/yabai").mkpath
+    man.mkpath
+
+    ENV.O2
+    system "make", "install"
+
+    bin.install "#{buildpath}/bin/yabai"
+    (pkgshare/"examples").install "#{buildpath}/examples/yabairc"
+    (pkgshare/"examples").install "#{buildpath}/examples/skhdrc"
+    man1.install "#{buildpath}/doc/yabai.1"
+  end
+
+  def caveats; <<~EOS
+    Copy the example configuration into your home directory:
+      cp #{opt_pkgshare}/examples/yabairc ~/.yabairc
+      cp #{opt_pkgshare}/examples/skhdrc ~/.skhdrc
+    Logs will be found in
+      #{var}/log/yabai/yabai.[out|err].log
+    EOS
+  end
+
+  plist_options :manual => "yabai"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/yabai</string>
+      </array>
+      <key>EnvironmentVariables</key>
+      <dict>
+        <key>PATH</key>
+        <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+      </dict>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/yabai/yabai.out.log</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/yabai/yabai.err.log</string>
+    </dict>
+    </plist>
+    EOS
+  end
+end
