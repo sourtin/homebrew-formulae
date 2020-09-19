f13c3811dfa7d371aecb81f406b684f39986bd53
William Earley <nitrous@sourt.in>
2020-09-19 17:37:49 +0100

    zathura icon

--- zathura.rb
+++ zathura.rb
@@ -8,6 +8,11 @@ class Zathura < Formula
   version "0.4.6"
   sha256 "d15e36d4a8f293c16766358ac4de3196c763453f87d31c7f9cfb00d3e281b76a"
 
+  resource "icon" do
+    url "https://gist.githubusercontent.com/sourtin/d44a598764277449e8f4bd5834b1f729/raw/eed970211ac6fd08c88403d8382d07087e3b7061/zathura.b64"
+    sha256 "76b9f7e9acb4278fb2f680b140a1147c4b862482daeac174ee69f5f1d8068256"
+  end
+
   depends_on 'pkg-config'
   depends_on "libmagic"
   depends_on "gettext"
@@ -30,6 +35,16 @@ class Zathura < Formula
     system 'mkdir build'
     system "meson build --prefix #{prefix}"
     system "cd build && ninja && ninja install"
+  end
+
+  def post_install
+    resource("icon").stage do
+      system "base64 -d zathura.b64 > zathura.rsrc"
+      system "chmod", "u+w", "#{bin}/zathura"
+      system "Rez", "-append", "zathura.rsrc", "-o", "#{bin}/zathura"
+      system "SetFile", "-a", "C", "#{bin}/zathura"
+      system "chmod", "u-w", "#{bin}/zathura"
+    end
   end
 
   test do
